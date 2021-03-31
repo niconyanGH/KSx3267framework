f5afa6fcb97d823e14740a312de5cd860f051b62
niconyanGH <36921425+niconyanGH@users.noreply.github.com>
2021-03-31 16:41:33 +0900

    testing..

--- Java/SDKforKSX3267/src/KSX_CommSpec.java
+++
@@ -1,4 +0,0 @@
-
-public class KSX_CommSpec {
-    public KSX_3267 KS_X_3267_2018 ;
-}
--- Java/SDKforKSX3267/src/reg_write.java
+++
@@ -1,8 +0,0 @@
-
-import java.util.List;
-
-public class reg_write {
-    public int starting_register ;
-
-    public List<String> items;
-}
