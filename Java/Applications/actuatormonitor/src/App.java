1451c9e9fe3ad4344b5a145ac3a6c1b0cb151f58
niconyanGH <36921425+niconyanGH@users.noreply.github.com>
2021-03-31 17:37:46 +0900

    nhs 소스 수정

--- Java/Applications/actuatormonitor/src/App.java
+++ Java/Applications/actuatormonitor/src/App.java
@@ -13,6 +13,7 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 //kbm 소스 수정
+//nhs 소스 수정
 public class App extends Application {
 
     public ComboBox cbportList;
